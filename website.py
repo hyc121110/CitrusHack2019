2e52ff3d1d89077de69ed4e357c3366210ca0e6d
Tu Vu <tuvu1297@gmail.com>
2019-04-27 13:42:19 -0700

    website updated

--- website.py
+++ website.py
@@ -12,9 +12,24 @@ def welcome():
 def orgLogin():
     return render_template("/organizer/orgLogin.html")
 
-
 @app.route("/userSignUp")
 def userSignUp():
     return render_template("/user/signUp.html")
+
+@app.route("/schedule")
+def schedule():
+    return render_template("schedule.html")
+
+@app.route("/orgOpenTime")
+def orgOpenTime():
+    return render_template("/organizer/orgOpenTime.html")    
+
+@app.route("/orgUserInfo")
+def orgUserInfo():
+    return render_template("/organizer/orgUserInfo.html")  
+
+@app.route("/logout")
+def logout():
+    return render_template("/logout.html")  
 
 app.run()
